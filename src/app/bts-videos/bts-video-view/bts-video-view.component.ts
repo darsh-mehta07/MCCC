c2011704e8504f918a9cfd1e64af05e1ef25ac53
Harshit singh <harshit.singh@wdipl.com>
2021-09-17 22:34:36 +0530

    first

--- src/app/bts-videos/bts-video-view/bts-video-view.component.ts
+++ src/app/bts-videos/bts-video-view/bts-video-view.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import {BtsVideosService} from '../../_service/bts-videos.service';
 import { SafePipe } from '../../_config/safe.pipe';
 import { DomSanitizer } from '@angular/platform-browser';
-
+import { Config } from '../../_config/config';
 @Component({
   selector: 'app-bts-video-view',
   templateUrl: './bts-video-view.component.html',
@@ -14,8 +14,13 @@ export class BtsVideoViewComponent implements OnInit {
   btsVideoId: any;
   dataLoad: any = false;
   expanded = 0;
+  hostUrl:string = Config.Host+'backend2/';
   constructor(private actRoute:ActivatedRoute,
-    private route : Router,private btsVideosService: BtsVideosService,private dom:DomSanitizer) { }
+    private route : Router,private btsVideosService: BtsVideosService,private dom:DomSanitizer) {
+      this.actRoute.paramMap.subscribe((params: ParamMap) => {  
+        this.ngOnInit();
+      });
+     }
     BtsVideos:any;
     vid: any;
     desc: any;
@@ -29,9 +34,9 @@ export class BtsVideoViewComponent implements OnInit {
       this.btsVideoId = params.get('id');
       console.log(params );
       console.log( this.btsCategoryId );
+      // this.ngOnInit();
     });
-
-    this.btsVideosService.bts_videos_by_id({'video_id': this.btsVideoId})
+ this.btsVideosService.bts_videos_by_id({'video_id': this.btsVideoId})
           .subscribe(
                     data => { 
                         console.log(data);
@@ -40,7 +45,7 @@ export class BtsVideoViewComponent implements OnInit {
                         // this.vid = this.dom.bypassSecurityTrustResourceUrl(this.BtsVideos[0].video_url); 
                         this.vid = this.BtsVideos[0].video_url;
                         this.desc = this.BtsVideos[0].description;
-                    }); 
+                      }); 
 
     this.btsVideosService.get_bts_videos({'limit': null,'category_id':this.btsCategoryId})
           .subscribe(
