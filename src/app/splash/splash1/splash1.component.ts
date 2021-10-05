1c70560937c847858fa537d1864c51408bdf0168
BabluDMaurya <bablu.wdipl@gmail.com>
2021-10-05 13:15:07 +0530

    splash slider update

--- src/app/splash/splash1/splash1.component.ts
+++ src/app/splash/splash1/splash1.component.ts
@@ -1,4 +1,6 @@
+import { JsonpClientBackend } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-splash1',
@@ -6,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./splash1.component.css']
 })
 export class Splash1Component implements OnInit {
-
+  slides = [
+    {"no": 1,class1:"casting",class2:"casting-content",class3:"slide-btn1",img: "../../assets/img/casting-svg.svg",title:"Casting",des:"Get Updates on the most recent casting calls of feature films, TV commercials and other mediums. Register NOW!",link:"/splash2"},
+    {"no": 2,class1:"traning",class2:"traning-content",class3:"slide-btn2",img: "../../assets/img/traning.svg",title:"Training",des:"Don't stop growing. Never limit your acting abilities. Our Acting training will provide you the challenge that you as an actor desperately require.",link:"/splash3"},
+    {"no": 3,class1:"event",class2:"event-content",class3:"slide-btn3",img: "../../assets/img/events.svg",title:"Events",des:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s.",link:"/welcome"}
+  ];
   constructor() { }
+  //-----slick slider------------//    
+  slideConfig = {"slidesToShow": 1, "slidesToScroll": 1,"dots": false,"autoplay": false,
+  "autoplaySpeed": 3000,method:{
+    "slickPause" : true
+  }};
+  // addSlide() {
+  //   this.slides.push({img: "http://placehold.it/350x150/777777"})
+  // }    
+  // removeSlide() {
+  //   this.slides.length = this.slides.length - 1;
+  // }    
+  slickInit(e:any) {
+    // console.log('slick initialized');
+  }    
+  breakpoint(e:any) {
+    // console.log('breakpoint');
+  }    
+  afterChange(e:any) {
+    if(e.last){
+      this.slideConfig.autoplay = false;
+    }else{
+        console.log("after change");
+    }
+  }    
+  beforeChange(e:any) {
+    // console.log('beforeChange');
+  } 
+  slickPause(){
 
+  }
+  //-----slick slider------------//
   ngOnInit(): void {
   }
 
